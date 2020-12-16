2851c90d45f2d071e0a481a89a874227b13ffacb
Katelinn Jones <katiejonesyo@gmail.com>
2020-12-16 15:08:31 -0800

    sql done and server setup

---
+++ sql/setup.sql
@@ -0,0 +1,11 @@
+
+DROP TABLE IF EXISTS books;
+
+CREATE TABLE books (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    title TEXT NOT NULL,
+    cover_image TEXT NOT NULL,
+    rating INTEGER NOT NULL,
+    price INTEGER NOT NULL,
+    in_stock BOOLEAN NOT NULL
+)
