750ba7ee7493c101b9fb710ac03668bb0a4ce6bf
Katelinn Jones <katiejonesyo@gmail.com>
2020-12-16 16:11:16 -0800

    store.js done but server is not working

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
-const app = require("./lib/app");
-const pool = require("./lib/utils/pool");
+const app = require('./app');
+const pool = require('./utils/pool');
+const port = 3002;
 
-const PORT = process.env.PORT || 3002;
-
-app.listen(PORT, () => {
-  // eslint-disable-next-line no-console
-  console.log(`Started on ${PORT}`);
+app.listen(port, () => {
+    console.log(`listening at http://localhost:${port}`);
 });
 
 process.on("exit", () => {
